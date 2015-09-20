5ca62b4c7311162e47ca4a5d7db4e2df8a120b2e
NaevaTheCat <rachaelnaeva@gmail.com>
2015-09-20 23:33:13 +1000

    just added comments

--- src/Chat/Bot/Cipher.hs
+++ src/Chat/Bot/Cipher.hs
@@ -4,20 +4,38 @@ import Chat.Data
 import Data.List
 
 
--- LEVEL: Easy
---
+-- LEVEL: Easy?
 
+-- Goal is to implement a bot that takes a number and a string
+-- and returns a string with the characters ciphered by RotN 
+-- where N is the integer supplied.
+
+-- There are a lot of ways to do this, pitfalls / PITA issues
+-- if participants take the wrong approach.
+-- key ones I identify:
+-- negative integers (handled by mod in my example bot)
+-- integers greater than 26 (as above)
+-- punctuation
+-- capitalisation
+-- inelgant rotation e.g. excessive type conversions/lookups
+
+-- Below is an example I feel is elegant
 type Shift = Int
 type Phrase = String
 
-alphLower = ['a'..'z']
-alphUpper = ['A'..'Z']
+cipher :: Char -> [(Char, Char)] -> Char
+cipher c list = maybe c id $ lookup c list
 
 cipherTable :: Int -> [(Char, Char)]
 cipherTable n = zip alphLower shiftLow ++ zip alphUpper shiftUpper where
     shiftLow = drop n alphLower ++ take n alphLower
     shiftUpper = drop n alphUpper ++ take n alphUpper
 
+alphLower = ['a'..'z']
+alphUpper = ['A'..'Z']
+
+----- most of this can be ignored. If this example is to teach map
+----- it may be worth having them poke at the guts or maybe restructuring
 cipherBot :: Bot
 cipherBot m = maybe (return Nothing) encipher $ stripPrefix "/cipher " m
     where encipher m' =  let (nS,rest) = break (==' ') m'
@@ -25,5 +43,4 @@ cipherBot m = maybe (return Nothing) encipher $ stripPrefix "/cipher " m
                              list = cipherTable n
                          in return $ Just $ map (flip cipher list) rest
 
-cipher :: Char -> [(Char, Char)] -> Char
-cipher c list = maybe c id $ lookup c list
+

3cf849d7d285c090da95e574789994d70bc582f8
Charles O'Farrell <charleso@charleso.org>
2015-10-31 10:57:09 +1100

    Remove unused bot imports

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,6 @@ import           Chat.Bot.Answer.Guess
 import           Chat.Bot.Answer.Cipher
 import           Chat.Bot.Answer.Calculator
 import           Chat.Bot.Answer.TicTacToe
-import           Chat.Bot.Cipher
-import           Chat.Bot.Calculator
-import           Chat.Bot.Guess
-import           Chat.Bot.TicTacToe
 import           Chat.Data
 import           Chat.Web
 

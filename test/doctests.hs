2c1dbaefcc88284502ffc3fa3b8e7ea217fd4acf
Charles O'Farrell <charleso@charleso.org>
2015-12-05 17:31:05 +1100

    Import everything in test for ghci

--- test/doctests.hs
+++ test/doctests.hs
@@ -2,6 +2,40 @@ module Main where
 
 import           Build_doctests (deps)
 import           Chat
+
+import           Chat.Bot.Calculator
+import           Chat.Bot.Misc.Calculator
+import           Chat.Bot.Answer.Calculator
+import           Chat.Bot.Run.Calculator
+
+import           Chat.Bot.Ping
+import           Chat.Bot.Answer.Ping
+
+import           Chat.Bot.Guess
+import           Chat.Bot.Misc.Guess
+import           Chat.Bot.Answer.Guess
+import           Chat.Bot.Run.Guess
+
+import           Chat.Bot.Hangman
+import           Chat.Bot.Misc.Hangman
+import           Chat.Bot.Answer.Hangman
+import           Chat.Bot.Run.Hangman
+
+import           Chat.Bot.Vote
+import           Chat.Bot.Misc.Vote
+import           Chat.Bot.Answer.Vote
+import           Chat.Bot.Run.Vote
+
+import           Chat.Bot.Cipher
+import           Chat.Bot.Misc.Cipher
+import           Chat.Bot.Answer.Cipher
+import           Chat.Bot.Run.Cipher
+
+import           Chat.Bot.TicTacToe
+import           Chat.Bot.Misc.TicTacToe
+import           Chat.Bot.Answer.TicTacToe
+import           Chat.Bot.Run.TicTacToe
+
 import           Control.Applicative
 import           Control.Monad
 import           Data.Char
